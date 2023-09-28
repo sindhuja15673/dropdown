edb96c6e64dc1790d2c5331f90db98f810319a1f
sindhuja15673 <132433742+sindhuja15673@users.noreply.github.com>
2023-09-28 05:17:50 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+
+import React, { useEffect, useState } from "react";
+const cities = ['Ariyalur','bangalore','bombay', 'cuddalore', 'chennai','coimbatore', 'vadalur', 'Chidambaram'];
+
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [filteredCities, setFilteredCities] = useState([]);
+
+  const onChangeHandler = (e) => {
+    const inputText = e.target.value;
+    setSearchText(inputText);
+
+    // Filter cities based on the input text
+    const filtered = cities.filter(element => element.toLowerCase().includes(inputText.toLowerCase()));
+    setFilteredCities(filtered);
+  }
+  useEffect(()=>{
+            setFilteredCities([...cities.filter(element=> element?.startsWith(searchText))])
+    
+        },[searchText])
+
+  return (
+    <>
+      <input
+        value={searchText}
+        onChange={onChangeHandler}
+        placeholder="Search for a city"
+      />
+      
+      {searchText && filteredCities.length > 0 && (
+        
+        <div className="dropdown">
+          
+            
+            {filteredCities.map((element, idx) => (
+              <option key={idx} onClick={() => setSearchText(element)}> 
+                {element}
+             </option>
+            ))}
+          
+         
+          
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
+
